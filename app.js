e7e4f8b894fdccaa77f7fc3088356f7825ba6655
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-06-26 01:23:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,9 +24,23 @@ octokit.gists.update({
         "description": "Live Data",
         "files": {
             "Live": {
-                "content": "Good Evening Guys... 🌝 \nI am a running this script 🌝 \n"+ res +"\nto live update this section. 🌝 \nIsn't it awesome?"
+                "content": "Good Evening Guys... 🌝 \nI am a running this script 🌝 \n"+ getDate() +"\nto live update this section. 🌝 \nIsn't it awesome?"
             }
         }
     }).then(res => {
         console.log(res.data);
     });
+
+
+function getDate() {
+    // current timestamp in milliseconds
+    let ts = Date.now();
+
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    // prints date & time in YYYY-MM-DD format
+    return year + "-" + month + "-" + date;
+}
