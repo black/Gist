5a6c7f32775b65a1534f468928b0c83d087190f4
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-06-26 01:26:12 -0400

    updated

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const { Octokit } = require("@octokit/rest"),
 // });
 
 const {
-  GIST_ID: gistId,
-  GH_TOKEN: githubToken, 
+    GIST_ID: gistId,
+    GH_TOKEN: githubToken,
 } = process.env;
 const API_BASE = 'https://api.github.com/gists/';
 const username = 'black';
@@ -25,16 +25,29 @@ var rule = new schedule.RecurrenceRule();
 //rule.minute = 40;
 rule.second = 10;
 var jj = schedule.scheduleJob(rule, function(res) {
-    console.log("execute jj", res); 
+    console.log("execute jj", res);
     octokit.gists.update({
         gist_id: gistID,
         "description": "Live Data",
         "files": {
             "Live": {
-                "content": "Good Evening Guys... 🌝 \nI am a running this script 🌝 \n"+ res +"\nto live update this section. 🌝 \nIsn't it awesome?"
+                "content": "Good Evening Guys... 🌝 \nI am a running this script 🌝 \n" + getDate() + "\nto live update this section. 🌝 \nIsn't it awesome?"
             }
         }
     }).then(res => {
         console.log(res.data);
     });
 });
+
+function getDate() {
+    // current timestamp in milliseconds
+    let ts = Date.now();
+
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    // prints date & time in YYYY-MM-DD format
+    return year + "-" + month + "-" + date;
+}
